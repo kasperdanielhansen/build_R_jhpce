a4ee3f5a3ac0e949761ea27af90d24ca8d0f8ff8
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2019-09-19 09:38:52 -0400

    Rprofile

---
+++ deploy_Rprofile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e -u
+cp Rprofile.site /jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/svnR-3.6/R/3.6/lib64/R/etc
+cp Rprofile.site /jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/svnR-3.6.x/R/3.6.x/lib64/R/etc
