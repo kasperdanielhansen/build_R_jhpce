d379d52f7f05cb8aad20bba0e6471e2b32cfb698
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2021-04-30 03:18:45 -0400

    build

---
+++ build.conda.svnR-4.0.x.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#$ -V -j yes -S /bin/bash
+#$ -m n -M kasperdanielhansen@gmail.com
+
+set -e
+
+R_VERSION=4.0.x
+SVNLOC=https://svn.r-project.org/R/branches/R-4-0-branch
+
+## module load conda_R/${R_VERSION}
+module load conda_R/${R_VERSION}
+module load conda
+CONDA_PREFIX=`conda info --json | grep default_prefix | sed 's|  "default_prefix": "||' | sed 's|",$||'`
+export PATH=${CONDA_PREFIX}/texlive/bin/x86_64-linux:${PATH} # Required in case custom texlive is not at the root of the PATH
+
+SRCDIR=R-${R_VERSION}-src
+BUILDDIR=R-${R_VERSION}-build
+
+if [ ! -d ${SRCDIR} ]; then
+    svn co ${SVNLOC}  ${SRCDIR}
+fi
+cd ${SRCDIR}
+svn up || exit 1
+tools/rsync-recommended || exit 1
+tools/link-recommended || exit 1
+cd ..
+rm -rf ${BUILDDIR}
+mkdir ${BUILDDIR}
+cd ${BUILDDIR}
+export LANG=en_US.UTF-8
+export TZ="US/Eastern"
+../${SRCDIR}/configure SHELL='/bin/bash' \
+   --prefix="${CONDA_PREFIX}/R/${R_VERSION}" \
+   CC="x86_64-conda_cos6-linux-gnu-gcc" \
+   CXX="x86_64-conda_cos6-linux-gnu-g++" \
+   F77="x86_64-conda_cos6-linux-gnu-gfortran" \
+   FC="$F77" \
+   CFLAGS="-Wall -mtune=amdfam10 -g -O2 -I${CONDA_PREFIX}/include"\
+   CXXFLAGS="-Wall -mtune=amdfam10 -g -O2 -I${CONDA_PREFIX}/include" \
+   F77FLAGS="-Wall -g -O2 -mtune=amdfam10 -I${CONDA_PREFIX}/include" \
+   FCFLAGS="$F77FLAGS" \
+   LDFLAGS="-L${CONDA_PREFIX}/lib -Wl,-rpath-link=${CONDA_PREFIX}/lib" \
+   --with-tcl-config=${CONDA_PREFIX}/lib/tclConfig.sh\
+   --with-tk-config=${CONDA_PREFIX}/lib/tkConfig.sh\
+   --enable-R-shlib\
+   --enable-memory-profiling | tee ../configure-${R_VERSION}
+make
+make check
+make install
+
+INSTALL_DIR=${CONDA_PREFIX}/R/${R_VERSION}/lib64/R
+mkdir -p ${INSTALL_DIR}/site-library
+cp /users/khansen/build/build_R_jhpce/Rprofile.site ${INSTALL_DIR}/etc
---
+++ build.conda.svnR-devel.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#$ -V -j yes -S /bin/bash
+#$ -m n -M kasperdanielhansen@gmail.com
+
+set -e
+
+R_VERSION=devel
+SVNLOC=https://svn.r-project.org/R/trunk
+
+## module load conda_R/${R_VERSION}
+module load conda_R/${R_VERSION}
+module load conda
+CONDA_PREFIX=`conda info --json | grep default_prefix | sed 's|  "default_prefix": "||' | sed 's|",$||'`
+export PATH=${CONDA_PREFIX}/texlive/bin/x86_64-linux:${PATH} # Required in case custom texlive is not at the root of the PATH
+
+SRCDIR=R-${R_VERSION}-src
+BUILDDIR=R-${R_VERSION}-build
+
+if [ ! -d ${SRCDIR} ]; then
+    svn co ${SVNLOC}  ${SRCDIR}
+fi
+cd ${SRCDIR}
+svn up || exit 1
+tools/rsync-recommended || exit 1
+tools/link-recommended || exit 1
+cd ..
+rm -rf ${BUILDDIR}
+mkdir ${BUILDDIR}
+cd ${BUILDDIR}
+export LANG=en_US.UTF-8
+# export TZ="US/Eastern"
+../${SRCDIR}/configure SHELL='/bin/bash' \
+   --prefix="${CONDA_PREFIX}/R/${R_VERSION}" \
+   CC="x86_64-conda_cos6-linux-gnu-gcc" \
+   CXX="x86_64-conda_cos6-linux-gnu-g++" \
+   F77="x86_64-conda_cos6-linux-gnu-gfortran" \
+   FC="$F77" \
+   CFLAGS="-Wall -mtune=amdfam10 -g -O2 -I${CONDA_PREFIX}/include"\
+   CXXFLAGS="-Wall -mtune=amdfam10 -g -O2 -I${CONDA_PREFIX}/include" \
+   F77FLAGS="-Wall -g -O2 -mtune=amdfam10 -I${CONDA_PREFIX}/include" \
+   FCFLAGS="$F77FLAGS" \
+   LDFLAGS="-L${CONDA_PREFIX}/lib -Wl,-rpath-link=${CONDA_PREFIX}/lib" \
+   --enable-R-shlib\
+   --with-tcl-config=${CONDA_PREFIX}/lib/tclConfig.sh\
+   --with-tk-config=${CONDA_PREFIX}/lib/tkConfig.sh\
+   --enable-memory-profiling | tee ../configure-${R_VERSION}
+make
+make check
+make install
