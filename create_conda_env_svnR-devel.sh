c8471f7c26aa8ee61041ed8225e8bca2c26c24d3
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2021-04-30 03:18:01 -0400

    creating conda

---
+++ create_conda_env_svnR-devel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+CONDA_VERSION=3-4.6.14
+R_NAME=svnR-devel
+
+module load conda/${CONDA_VERSION}
+conda env create --prefix /jhpce/shared/jhpce/core/conda/miniconda${CONDA_VERSION}/envs/${R_NAME} -f conda_${R_NAME}.yml
+
+
+
+#conda activate /jhpce/shared/jhpce/core/conda/miniconda${CONDA_VERSION}/envs/${R_NAME}
+
+## The following packages gets grabbed from conda-forge
+#conda install -c conda-forge --file conda_${R_NAME}_pkglist.txt
+
+## We now install texlive, since texlive from conda is broken
